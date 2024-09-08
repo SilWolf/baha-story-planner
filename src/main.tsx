857c7b5332a2b2e9765a5395b4faeac665e12eec
dicky.c <dicky.c@binance.com>
2024-09-08 21:07:56 +0800

    chore: install react router

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,22 @@ import { createRoot } from "react-dom/client";
 
 import { ThemeProvider } from "@material-tailwind/react";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>
 );
---
+++ src/pages/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@material-tailwind/react";
+
+export default function RootLandingPage() {
+  return (
+    <div className="container mx-auto text-center">
+      <Button fullWidth>登入</Button>
+    </div>
+  );
+}
