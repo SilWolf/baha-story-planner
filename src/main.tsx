f84377444b32ea7820fd6c00e3c041bed95ad9b0
dicky.c <dicky.c@binance.com>
2024-09-09 14:31:12 +0800

    feat: install daisyui

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import "./index.css";
 
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { themeChange } from "theme-change";
 
 import router from "./router.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+const Main = () => {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
+
+  return (
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+};
+
+createRoot(document.getElementById("root")!).render(<Main />);
--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,5 +1,25 @@
+import { Button, Card } from "react-daisyui";
+
 export default function RootLandingPage() {
   return (
-    <div className="container mx-auto text-center flex items-center justify-center"></div>
+    <div className="container mx-auto text-center flex items-center justify-center h-screen">
+      <Card>
+        <Card.Body>
+          <div className="space-y-2">
+            <a href="/app/stories/1">
+              <Button fullWidth variant="outline">
+                以 Google 帳號登入
+              </Button>
+            </a>
+            <Button fullWidth variant="outline">
+              以 Discord 帳號登入
+            </Button>
+            <Button fullWidth variant="outline">
+              以巴哈姆特帳號登入
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
